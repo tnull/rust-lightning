5665ca7dc423aeae80bb04d6b69c01c71edef3e5
Elias Rohrer <ero@tnull.de>
2022-11-23 09:33:37 +0100

    Add transaction sync crate
    This crate provides utilities for syncing LDK via the transaction-based
    `Confirm` interface. The initial implementation facilitates
    synchronization with an Esplora backend server.

---
+++ lightning-transaction-sync/src/error.rs
@@ -0,0 +1,30 @@
+use std::fmt;
+
+#[derive(Debug)]
+/// An error that possibly needs to be handled by the user.
+pub enum TxSyncError {
+	/// A transaction sync failed.
+	Failed,
+	/// An inconsisteny was encounterd during transaction sync.
+	Inconsistency,
+}
+
+impl fmt::Display for TxSyncError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			Self::Failed => write!(f, "Failed to conduct transaction sync."),
+			Self::Inconsistency => {
+				write!(f, "Encountered an inconsisteny during transaction sync.")
+			}
+		}
+	}
+}
+
+impl std::error::Error for TxSyncError {}
+
+#[cfg(any(feature = "esplora-blocking", feature = "esplora-async"))]
+impl From<esplora_client::Error> for TxSyncError {
+	fn from(_e: esplora_client::Error) -> Self {
+		Self::Failed
+	}
+}
